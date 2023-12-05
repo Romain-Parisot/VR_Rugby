e3dd9e9c8056daf874eea96ce61b757dac80b557
Rowars2 <romain.parisot@edu.devinci.fr>
2023-12-05 16:37:25 +0100

    wip

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
-const video = document.querySelector('#video');
-
-function EventVideo() {
-    video.pause();
-}
-
-setInterval(EventVideo, 3000);
+document.addEventListener('DOMContentLoaded', function() {
+    const video = document.getElementById('video');
+    const overlayText = document.querySelectorAll('#overlayText');
+  
+    let countdown = 4;
+    let timer;
+  
+    video.addEventListener('loadedmetadata', function() {
+      startTimer();
+    });
+  
+    function startTimer() {
+      setTimeout(() => {
+        clearInterval(timer);
+        countdown = 4;
+  
+        overlayText.forEach(el => {
+            el.setAttribute('visible', 'true');
+        });
+        overlayText.forEach(el => {
+            el.setAttribute('text', {
+                value: countdown,
+                color: 'white',
+                align: 'center',
+              });
+        })
+  
+        timer = setInterval(() => {
+          countdown -= 1;
+          if (countdown >= 0) {
+            overlayText.setAttribute('text', { value: countdown });
+          } else {
+            clearInterval(timer);
+            overlayText.setAttribute('visible', 'false');
+            video.play();
+          }
+        }, 1000);
+      }, 5000);
+    }
+  });
+  
