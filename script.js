057e5ec105118a917d253d9b3e994625b244514a
Rowars2 <romain.parisot@edu.devinci.fr>
2023-12-06 14:31:29 +0100

    pause with option to select

--- script.js
+++ script.js
@@ -1,40 +1,54 @@
 document.addEventListener('DOMContentLoaded', function() {
     const video = document.getElementById('video');
-    const overlayText = document.querySelectorAll('.pauseTimer');
+    const overlayText = document.querySelector('.pauseTimer');
+    const messageChoose = document.querySelector('.messageChoose');
+    const messageOption1 = document.querySelector('.messageOption1');
+    const messageOption2 = document.querySelector('.messageOption2');
 
     let countdown = 4;
     let timer;
+    let hasRun = false;
 
-    video.addEventListener('loadedmetadata', function() {
-        startTimer();
+    video.addEventListener('play', function() {
+        if (!hasRun) {
+            hasRun = true;
+            startTimer();
+        }
     });
 
     function startTimer() {
         setTimeout(() => {
         clearInterval(timer);
         countdown = 4;
-        console.log(overlayText);
         video.pause();
-        overlayText.forEach(el => {
-            el.classList.toggle('hide');
-            let countdownValue = document.createTextNode(countdown);
-            el.appendChild(countdownValue);            
-        });
+        messageChoose.classList.toggle('hide');
+        messageOption1.classList.toggle('hide');
+        messageOption2.classList.toggle('hide');
+
+        let chooseOptionValue = document.createTextNode("Choisissez une option:");
+        let option1 = document.createTextNode("1. Faire la passe a gauche");
+        let option2 = document.createTextNode("2. Traversez la defense");
+
+        messageChoose.appendChild(chooseOptionValue);
+        messageOption1.appendChild(option1);
+        messageOption2.appendChild(option2);
+
+        overlayText.classList.toggle('hide');
+        let countdownValue = document.createTextNode(countdown);
+        overlayText.appendChild(countdownValue);
 
         timer = setInterval(() => {
             countdown -= 1;
             if (countdown >= 0) {
-                overlayText.forEach(el => {
-                    el.textContent = '';
-                    let countdownValue = document.createTextNode(countdown);
-                    el.appendChild(countdownValue);
-                    console.log(countdown);
-                });
+                overlayText.textContent = '';
+                let countdownValue = document.createTextNode(countdown);
+                overlayText.appendChild(countdownValue);
             } else {
             clearInterval(timer);
-            overlayText.forEach(el => {
-                el.classList.toggle('hide');           
-            });
+            overlayText.classList.toggle('hide');
+            messageChoose.classList.toggle('hide');
+            messageOption1.classList.toggle('hide');
+            messageOption2.classList.toggle('hide');
             video.play();
             }
         }, 1000);
@@ -42,8 +56,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 });
 
-
-function dpNone(){
-    var entity = document.querySelector('#playButton');
-    entity.classList.add('hide');
-}
+document.addEventListener('click', function() {
+    let element = document.querySelector('#playButton');
+    element ? element.remove() : null;
+});
